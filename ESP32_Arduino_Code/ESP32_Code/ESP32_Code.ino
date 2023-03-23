87077132e64ddca744e0e8e8ced25474ae3e9419
Christopher Jenner <49998052+ChrisJLKC@users.noreply.github.com>
2023-03-23 16:53:03 +0000

    Added code for Bluetooth Module

--- ESP32_Arduino_Code/ESP32_Code/ESP32_Code.ino
+++ ESP32_Arduino_Code/ESP32_Code/ESP32_Code.ino
@@ -14,15 +14,21 @@ void RECV() {
 }
 
 void READ_FILE() {
-  SerialBT.println("1");
+  uint8_t msg = 1;
+  SerialBT.write(msg);
+  SerialBT.println(1);
+  while (!SerialBT.available());
+
+  if (SerialBT.available()) {
+    String Recv_data = SerialBT.readString();
+    Serial.println(Recv_data);
+  }
 }
 
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32");
   Serial.println("Bluetooth device is ready to pair");
-  READ_FILE();
-  Serial.println("Opened File.");
 }
 
 void loop() {
